187551837c487ae9a94aa537032f650232926f0b
VOIDRLM <varjun609@gmail.com>
2023-09-03 17:49:24 +0530

    Implementation of post method and code changes

--- db/users.py
+++ db/users.py
@@ -1,6 +1,1 @@
-data_list = [
-    {"name": "AAA", "age": 30, "email": "AAA@example.com"},
-    {"name": "BBB", "age": 30, "email": "BBB@example.com"},
-    {"name": "CCC", "age": 30, "email": "CCC@example.com"},
-    {"name": "DDD", "age": 30, "email": "DDD@example.com"},
-]
+data = []
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from flask import Flask, jsonify
-
-from db.users import data_list
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-print("Local Server running...")
+from db.users import data
 
 
-@app.route("/get_data", methods=["GET"])
-def get_data():
-    try:
-        return jsonify(data_list)
-    except:
-        # Handle the exception
-        print("error")
+@app.route("/api/get_all", methods=["GET"])
+@app.route("/api/add_item", methods=["POST"])
+def handle_data():
+    if request.method == 'GET':
+        output = {'status': 'success', 'data': data}
+        return jsonify(output)
+    elif request.method == 'POST':
+        new_item = request.json
+        data.append(new_item)
+        output = {'status': 'success'}
+        return jsonify(output)
 
 
 if __name__ == "__main__":
-    app.run(debug=True),
+    app.run(debug=True)
